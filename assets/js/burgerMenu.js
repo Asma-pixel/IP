9e103b1081f6b048d55c6fda83fe203622a854f0
Asma-pixel <serafimcomru91@gmail.com>
2021-04-25 02:11:19 +0300

    header with burger

--- assets/js/burgerMenu.js
+++ assets/js/burgerMenu.js
@@ -1,3 +1,17 @@
 export function burgerMenu() {
-    
+    let burger = document.querySelector(".burger");
+    let nav = document.querySelector(".middle-side");
+    let application = document.querySelector(".application");
+
+    burger.addEventListener("click", (e)=>{
+        burger.classList.toggle("open");
+        nav.classList.toggle("open-middle-side");
+
+        if ( application.classList.contains("close-application")){
+            application.classList.add("open-application")
+        }
+
+        application.classList.toggle("close-application");
+
+    });
 }
